2a4df1ac9311721ca731ad0c61d14d074245ff24
Medgar24 <ALANADAZE@GMAIL.COM>
2025-04-10 01:57:01 -0500

    Added AuthController

--- Models/BillLink.cs
+++ Models/BillLink.cs
@@ -3,7 +3,6 @@ using fareShare.Models;
 
 namespace fareShare.Models;
 
-
 //Zian -> To link the bills with a list of user and make it easier to query with SQL later because we keep track of all the users that are linked to a bill
 
 public class BillLink
--- Repositories/BillRepository.cs
+++ Repositories/BillRepository.cs
@@ -1,132 +1,126 @@
-using fareShare.Models;
-using fareShare.Migrations;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using fareShare.Migrations;
+using fareShare.Models;
 using fareShare.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace fareShare.Repository;
 
 public class BillRepository : IBillRepository
 {
-        private readonly BillDbContext _context;
+    private readonly BillDbContext _context;
 
-        public BillRepository(BillDbContext context)
+    public BillRepository(BillDbContext context)
+    {
+        _context = context;
+    }
+
+    public Bill CreateBill(Bill bill)
+    {
+        if (bill == null)
         {
-            _context = context;
+            throw new ArgumentNullException(nameof(bill));
         }
 
-        public Bill CreateBill(Bill bill)
+        // Save the Bill
+        _context.Bill.Add(bill);
+        _context.SaveChanges();
+
+        // Automatically link the creator to the Bill using BillLink
+        if (bill.CreatorId == null)
         {
-            if (bill == null)
-            {
-                throw new ArgumentNullException(nameof(bill));
-            }
-
-            // Save the Bill
-            _context.Bill.Add(bill);
-            _context.SaveChanges();
-
-            // Automatically link the creator to the Bill using BillLink
-            if (bill.CreatorId == null)
-            {
-                throw new InvalidOperationException("CreatorId must be set when creating a bill.");
-            }
-
-            var billLink = new BillLink
-            {
-                BillId = bill.BillId,
-                UserId = bill.CreatorId.Value
-            };
-
-            _context.BillLink.Add(billLink);
-            _context.SaveChanges();
-
-            return bill;
+            throw new InvalidOperationException("CreatorId must be set when creating a bill.");
         }
 
-        public BillLink CreateBillLink(BillLink billLink)
+        var billLink = new BillLink { BillId = bill.BillId, UserId = bill.CreatorId.Value };
+
+        _context.BillLink.Add(billLink);
+        _context.SaveChanges();
+
+        return bill;
+    }
+
+    public BillLink CreateBillLink(BillLink billLink)
+    {
+        if (billLink == null)
         {
-            if (billLink == null)
-            {
-                throw new ArgumentNullException(nameof(billLink));
-            }
-
-            _context.BillLink.Add(billLink);
-            _context.SaveChanges();
-
-            return billLink;
+            throw new ArgumentNullException(nameof(billLink));
         }
 
-        public void DeleteBill(int id)
-        {
-            var bill = _context.Bill.Find(id);
-            if (bill == null)
-            {
-                throw new ArgumentNullException(nameof(bill));
-            }
+        _context.BillLink.Add(billLink);
+        _context.SaveChanges();
 
-            _context.Bill.Remove(bill); // Will also delete BillLinks due to cascade
-            _context.SaveChanges();
+        return billLink;
+    }
+
+    public void DeleteBill(int id)
+    {
+        var bill = _context.Bill.Find(id);
+        if (bill == null)
+        {
+            throw new ArgumentNullException(nameof(bill));
         }
 
-        public void DeleteBillLink(int billLinkId)
-        {
-            var link = _context.BillLink.Find(billLinkId);
-            if (link == null)
-            {
-                throw new ArgumentNullException(nameof(link));
-            }
+        _context.Bill.Remove(bill); // Will also delete BillLinks due to cascade
+        _context.SaveChanges();
+    }
 
-            _context.BillLink.Remove(link);
-            _context.SaveChanges();
+    public void DeleteBillLink(int billLinkId)
+    {
+        var link = _context.BillLink.Find(billLinkId);
+        if (link == null)
+        {
+            throw new ArgumentNullException(nameof(link));
         }
 
-        public Bill GetBill(int id)
+        _context.BillLink.Remove(link);
+        _context.SaveChanges();
+    }
+
+    public Bill GetBill(int id)
+    {
+        var bill = _context.Bill.Include(b => b.BillLinks).FirstOrDefault(b => b.BillId == id);
+
+        if (bill == null)
         {
-            var bill = _context.Bill
-                .Include(b => b.BillLinks)
-                .FirstOrDefault(b => b.BillId == id);
-
-            if (bill == null)
-            {
-                throw new ArgumentNullException(nameof(bill));
-            }
-
-            return bill;
+            throw new ArgumentNullException(nameof(bill));
         }
 
-        public List<Bill> GetBillsByUserId(int userId)
-        {
-            var bills = _context.BillLink
-                .Where(bl => bl.UserId == userId)
-                .Include(bl => bl.Bill)
-                .Select(bl => bl.Bill)
-                .ToList();
+        return bill;
+    }
 
-            return bills;
+    public List<Bill> GetBillsByUserId(int userId)
+    {
+        var bills = _context
+            .BillLink.Where(bl => bl.UserId == userId)
+            .Include(bl => bl.Bill)
+            .Select(bl => bl.Bill)
+            .ToList();
+
+        return bills;
+    }
+
+    public Bill UpdateBill(Bill updatedBill)
+    {
+        if (updatedBill == null)
+        {
+            throw new ArgumentNullException(nameof(updatedBill));
         }
 
-        public Bill UpdateBill(Bill updatedBill)
+        var existingBill = _context.Bill.Find(updatedBill.BillId);
+
+        if (existingBill == null)
         {
-            if (updatedBill == null)
-            {
-                throw new ArgumentNullException(nameof(updatedBill));
-            }
+            throw new ArgumentNullException(nameof(existingBill));
+        }
 
-            var existingBill = _context.Bill.Find(updatedBill.BillId);
+        existingBill.Name = updatedBill.Name;
+        existingBill.Description = updatedBill.Description;
+        existingBill.Price = updatedBill.Price;
 
-            if (existingBill == null)
-            {
-                throw new ArgumentNullException(nameof(existingBill));
-            }
-
-            existingBill.Name = updatedBill.Name;
-            existingBill.Description = updatedBill.Description;
-            existingBill.Price = updatedBill.Price;
-
-            _context.SaveChanges();
-            return existingBill;
+        _context.SaveChanges();
+        return existingBill;
     }
 }
