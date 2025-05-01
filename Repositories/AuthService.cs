741fafcd043a29fd92b29f96196f67d0120b595d
Medgar24 <ALANADAZE@GMAIL.COM>
2025-05-01 14:27:22 -0500

    profile pic Update works

--- Repositories/AuthService.cs
+++ Repositories/AuthService.cs
@@ -62,6 +62,11 @@ public class AuthService : IAuthService
         var claims = new Claim[]
         {
             new Claim(JwtRegisteredClaimNames.Sub, user.UserId.ToString()),
+            new Claim(
+                "profile_picture",
+                user.Img
+                    ?? "https://i.pinimg.com/originals/c0/9b/6d/c09b6d7edb7b4b89b382aa6ca0a761de.jpg"
+            ), //Maia ~ profile picture 'Custom' claim
             new Claim(JwtRegisteredClaimNames.Email, user.Email ?? ""),
             new Claim(JwtRegisteredClaimNames.FamilyName, user.LastName ?? ""),
             new Claim(JwtRegisteredClaimNames.GivenName, user.FirstName ?? ""),
@@ -70,7 +75,7 @@ public class AuthService : IAuthService
         // Create token
         var jwt = new JwtSecurityToken(
             claims: claims,
-            expires: DateTime.Now.AddMinutes(5),
+            expires: DateTime.Now.AddMinutes(30),
             signingCredentials: signingCredentials
         );
 
@@ -100,5 +105,22 @@ public class AuthService : IAuthService
         }
 
         return user;
+    }
+
+    public void UpdateProPic(int userId, string img)
+    {
+        var user = _context.Users.SingleOrDefault(u => u.UserId == userId);
+        if (user == null)
+            throw new Exception("User not found.");
+
+        user.Img = img;
+        _context.Users.Update(user);
+        _context.SaveChanges();
+    }
+
+    public void UpdateUser(User user)
+    {
+        _context.Users.Update(user);
+        _context.SaveChanges();
     }
 }
